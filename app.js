97b73f429a5e9092b59c060966779cbeb33a25f0
Quinn Ngo <quinnngo@usc.edu>
2017-03-15 14:38:40 -0700

    deleted useless stuff and added views directory

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+var express = require('express');
 var Yelp = require('yelp-api-v3');
 
 var yelp = new Yelp({
 	app_id: "txMAKnMIAeAOHElQpTEyuA",
 	app_secret: "4yJqQck1j6FrkAYNjE5TAcv3LRxDGrOX9Bqmv8Zrh0LClppgUFWHEhdK6IymsHdp"
 });
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
 
 yelp.search({ term: 'food', location: '90007'})
 .then(function (data){
@@ -12,3 +18,8 @@ yelp.search({ term: 'food', location: '90007'})
 .catch(function (err){
 	console.log(err);
 });
+
+
+app.listen(3000, function(){
+	console.log("Listening on port 3000!!!");
+})
