b04cfab416eafb46f6760ca60d033ab2522546c8
Quinn Ngo <quinnngo@usc.edu>
2017-03-15 14:47:51 -0700

    more packages

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var Yelp = require('yelp-api-v3');
+var bodyParser = require("body-parser");
 
 var yelp = new Yelp({
 	app_id: "txMAKnMIAeAOHElQpTEyuA",
 	app_secret: "4yJqQck1j6FrkAYNjE5TAcv3LRxDGrOX9Bqmv8Zrh0LClppgUFWHEhdK6IymsHdp"
 });
+
+var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
