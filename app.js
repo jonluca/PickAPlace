768862e29138127840771729118b7491450b4612
JonLuca DeCaro <jdecaro@usc.edu>
2017-03-15 14:57:49 -0700

    install path

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
 var express = require('express');
 var Yelp = require('yelp-api-v3');
 var bodyParser = require("body-parser");
+var path = require('path')
+
 
 var yelp = new Yelp({
-	app_id: "txMAKnMIAeAOHElQpTEyuA",
-	app_secret: "4yJqQck1j6FrkAYNjE5TAcv3LRxDGrOX9Bqmv8Zrh0LClppgUFWHEhdK6IymsHdp"
+  app_id: "txMAKnMIAeAOHElQpTEyuA",
+  app_secret: "4yJqQck1j6FrkAYNjE5TAcv3LRxDGrOX9Bqmv8Zrh0LClppgUFWHEhdK6IymsHdp"
 });
 
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-yelp.search({ term: 'food', location: '90007'})
-.then(function (data){
-	console.log(data);
+
+yelp.search({
+  term: 'food',
+  location: '90007'
 })
-.catch(function (err){
-	console.log(err);
-});
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
 
-app.get("/", function(req, res){
-	res.render("landing.ejs");
+app.get("/", function(req, res) {
+  res.render("landing.ejs");
 })
 
 
-app.listen(3000, function(){
-	console.log("Listening on port 3000!!!");
+app.listen(3000, function() {
+  console.log("Listening on port 3000!!!");
 })
