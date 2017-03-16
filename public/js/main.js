31630662817af2555c07bd8beba9591918769262
JonLuca DeCaro <jdecaro@usc.edu>
2017-03-15 20:55:40 -0700

    some refactors

--- public/js/main.js
+++ public/js/main.js
@@ -9,25 +9,74 @@ $(document).ready(function() {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
       },
-      success: function(data, code, jqXHR) {
-
-        /*Hide loading icon, title, and button in that order*/
-        $('.loading').css('display', 'none');
-        $('#title').css('display', 'none');
-        $('.searchpage').css('display', 'none');
-
-        var rating = data.results["rating"];
-        var distance = data.results["distance"];
-
-        console.log(distance);
-        /*Show three columns and new title*/
-        $('.choose').css('display', 'block');
-
-        $('.columns').css('display', 'block');
-
-
-      }
+      success: showRestaurants,
     });
+  }
+
+  function showRestaurants(data, code, jqXHR) {
+
+    /*Hide loading icon, title, and button in that order*/
+    $('.loading').css('display', 'none');
+    $('#title').css('display', 'none');
+    $('.searchpage').css('display', 'none');
+
+    var rating = data.results["rating"];
+    var distance = data.results["distance"];
+    var price = data.results["price"];
+    /*Show three columns and new title*/
+    $('.choose').css('display', 'block');
+
+    $('.columns').css('display', 'block');
+
+
+    $('#distanceImage').css("background-image", "url(" + distance["image_url"] + ")");
+    $('#ratingImage').css("background-image", "url(" + rating["image_url"] + ")");
+    $('#priceImage').css("background-image", "url(" + price["image_url"] + ")");
+
+    $('#goPrice').click(function() {
+      var name = price["name"];
+      var win = window.open('https://www.google.com/maps?q=' + encodeURIComponent(name), '_blank');
+      if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+      } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+      }
+    })
+
+    $('#goRating').click(function() {
+      var name = rating["name"];
+      var win = window.open('https://www.google.com/maps?q=' + encodeURIComponent(name), '_blank');
+      if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+      } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+      }
+    })
+
+    $('#goDistance').click(function() {
+      var name = distance["name"];
+      var win = window.open('https://www.google.com/maps?q=' + encodeURIComponent(name), '_blank');
+      if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+      } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+      }
+    })
+
+    $('#priceRest').text(price["name"])
+    $('#distRest').text(distance["name"])
+    $('#rateRest').text(rating["name"])
+
+    $('#distPrice').text("Price: " + distance["price"] + '\n' + distance["display_phone"] + '\n' + "Rating: " + distance["rating"] + '\n' + "Distance: " + convertToKM(distance["distance"]))
+    $('#ratePrice').text("Price: " + rating["price"] + '\n' + rating["display_phone"] + '\n' + "Rating: " + rating["rating"] + '\n' + "Distance: " + convertToKM(rating["distance"]))
+    $('#pricePrice').text("Price: " + price["price"] + '\n' + price["display_phone"] + '\n' + "Rating: " + price["rating"] + '\n' + "Distance: " + convertToKM(price["distance"]))
+
   }
 
   function convertToKM(meters) {
@@ -52,77 +101,11 @@ $(document).ready(function() {
           latitude: '34.0290238',
           longitude: '-118.2721847',
         },
-        success: function(data, code, jqXHR) {
-
-          /*Hide loading icon, title, and button in that order*/
-          $('.loading').css('display', 'none');
-          $('#title').css('display', 'none');
-          $('.searchpage').css('display', 'none');
-
-          var rating = data.results["rating"];
-          var distance = data.results["distance"];
-          var price = data.results["price"];
-          /*Show three columns and new title*/
-          $('.choose').css('display', 'block');
-
-          $('.columns').css('display', 'block');
-
-          console.log(rating)
-
-          $('#distanceImage').css("background-image", "url(" + distance["image_url"] + ")");
-          $('#ratingImage').css("background-image", "url(" + rating["image_url"] + ")");
-          $('#priceImage').css("background-image", "url(" + price["image_url"] + ")");
-
-          $('#goPrice').click(function() {
-            var name = price["name"];
-            var win = window.open('https://www.google.com/maps?q=' + encodeURIComponent(name), '_blank');
-            if (win) {
-              //Browser has allowed it to be opened
-              win.focus();
-            } else {
-              //Browser has blocked it
-              alert('Please allow popups for this website');
-            }
-          })
-
-          $('#goRating').click(function() {
-            var name = rating["name"];
-            var win = window.open('https://www.google.com/maps?q=' + encodeURIComponent(name), '_blank');
-            if (win) {
-              //Browser has allowed it to be opened
-              win.focus();
-            } else {
-              //Browser has blocked it
-              alert('Please allow popups for this website');
-            }
-          })
-
-          $('#goDistance').click(function() {
-            var name = distance["name"];
-            var win = window.open('https://www.google.com/maps?q=' + encodeURIComponent(name), '_blank');
-            if (win) {
-              //Browser has allowed it to be opened
-              win.focus();
-            } else {
-              //Browser has blocked it
-              alert('Please allow popups for this website');
-            }
-          })
-
-          $('#priceRest').text(price["name"])
-          $('#distRest').text(distance["name"])
-          $('#rateRest').text(rating["name"])
-
-          $('#distPrice').text("Price: " + distance["price"] + '\n' + distance["display_phone"] + '\n' + "Rating: " + distance["rating"] + '\n' + "Distance: " + convertToKM(distance["distance"]))
-          $('#ratePrice').text("Price: " + rating["price"] + '\n' + rating["display_phone"] + '\n' + "Rating: " + rating["rating"] + '\n' + "Distance: " + convertToKM(rating["distance"]))
-          $('#pricePrice').text("Price: " + price["price"] + '\n' + price["display_phone"] + '\n' + "Rating: " + price["rating"] + '\n' + "Distance: " + convertToKM(price["distance"]))
-
-        }
+        success: showRestaurants,
       });
     } else {
       button.innerHTML = "Geolocation is not supported by this browser.";
     }
 
   });
-
 });
