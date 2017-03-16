35f63c95804d6c2c1070db47ea6ac9f11a7fc081
JonLuca DeCaro <jdecaro@usc.edu>
2017-03-15 17:03:51 -0700

    Front end

--- public/js/main.js
+++ public/js/main.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 
         $('.columns').css('display', 'block');
 
-        $('#distanceImage').src = distance["businesses"][0]["image_url"];
+        $('#distanceImage').attr("src", distance["businesses"][0]["image_url"]);
 
       }
     });
