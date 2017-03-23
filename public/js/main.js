6456a9d820ef34476274caa07613fea0d73a0b59
JonLuca DeCaro <jdecaro@usc.edu>
2017-03-22 22:48:55 -0700

    Added error, changed color

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
     function locationFailed(err) {
-      console.log(err)
+        console.log(err);
+        $("#error").text("Unable to get location! Please try again!");
     }
 
     function sendLoc(position) {
-      $.ajax({
-        method: 'POST',
-        url: "/search",
-        type: 'json',
-        data: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        },
-        success: function(data, textStatus, jqXHR){
-          window.location = data.redirect
-        }
-      });
+        $.ajax({
+            method: 'POST',
+            url: "/search",
+            type: 'json',
+            data: {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            },
+            success: function(data, textStatus, jqXHR) {
+                window.location = data.redirect;
+            }
+        });
     }
 
     $('#search').click(function() {
-      // add loading bar
-      var button = document.getElementById('search');
+        // add loading bar
+        var button = document.getElementById('search');
 
-      $('.loading').css('display', 'block');
+        $('.loading').css('display', 'block');
 
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(sendLoc, locationFailed, {
-          timeout: 10000,
-          maximumAge: 180000
-        });
-      }
-      else {
-        button.innerHTML = "Geolocation is not supported by this browser.";
-      }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(sendLoc, locationFailed, {
+                timeout: 10000,
+                maximumAge: 180000
+            });
+        } else {
+            button.innerHTML = "Geolocation is not supported by this browser.";
+        }
     });
 });
